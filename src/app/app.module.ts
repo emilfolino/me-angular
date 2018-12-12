b3cdce3896cc54439e250b9daa36bc35e56271b6
Emil Folino <emilfolino@gmail.com>
2018-12-12 14:06:48 +0100

    added routing and components to fill routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeComponent } from './me/me.component';
+import { ReportComponent } from './report/report.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'me', component: MeComponent },
+  { path: 'report/:kmom', component: ReportComponent },
+  { path: '',
+    redirectTo: '/me',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeComponent,
+    ReportComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     AppRoutingModule
   ],
